963da451c784e7be6ee9f39b6b0fc0c0bf3e7125
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-05-25 15:11:33 -0600

    pulled APIdata from into top of browser

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,33 @@
 const APIKey = "feb4f2d7e33865553a2c80b01d7dd193";
 const searchBarEl = document.querySelector("#search-text");
 const searchBtn = document.querySelector("#searchBtn");
-const CityNameEl = document.getElementById("city");
-const todaysDateEl = document.getElementById("todays-date");
-const forecastIconEl = document.getElementById("forecast-icon");
+const cityDateIconEl = document.getElementById("city-date-icon");
 const currentTempEl = document.getElementById("current-temp");
-const WindSpeedEl = document.getElementById("wind-mph");
+const windSpeedEl = document.getElementById("wind-mph");
 const humidityEl = document.getElementById("humid");
 const uvIndexEl = document.getElementById("uv-number");
 
 function showForecast(event) {
   event.preventDefault();
   let city = searchBarEl.value;
-  let queryURL =
-    "http://api.openweathermap.org/data/2.5/weather?q=" +
-    city +
-    "&appid=" +
-    APIKey;
+    let queryURL =
+        "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
   fetch(queryURL)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
-      //need to use the DOM to manipulate the HTML for CityName, CurrentDate, Icon, Temp, Wind, Humidity, UV Index//
+        console.log(data);
+        console.log(data.main.temp)
+        console.log(data.name)
+        console.log(data.main.humidity)
+        let currentDate = moment.unix(data.dt).format("MMMM Do, YYYY");
+        console.log(currentDate)
+        cityDateIconEl.textContent = data.name + " " + currentDate; 
+        console.log(data.weather[0].icon)
+        currentTempEl.textContent = "Temp: " + data.main.temp + " °F";
+        windSpeedEl.textContent = "Wind: " + data.wind.speed + " Mph"
+        humidityEl.textContent = "Humidity: " + data.main.humidity + "%"
     });
 }
 searchBtn.addEventListener("click", showForecast);

95c3004f4d1b73ffca9a387f0de71580290f2457
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-05-26 16:32:33 -0600

    got my UVI to pull through!!

--- scripts/script.js
+++ scripts/script.js
@@ -9,7 +9,6 @@ const uvIndexEl = document.getElementById("uv-number");
 const fiveDayEl = document.getElementById("day-box");
 
 
-
 function getForecast(event) {
   event.preventDefault();
   let city = searchBarEl.value;
@@ -20,6 +19,7 @@ function getForecast(event) {
     })
     .then(function (data) {
       console.log(data);
+      let currentUVI 
       var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude={part}&appid=${APIKey}&units=imperial&cnt=5`;
       fetch(requestUrl)
         .then(function (response) {
@@ -27,12 +27,15 @@ function getForecast(event) {
         })
         .then(function (data) {
           console.log(data);
+          let objTwo = data
+          currentUVI = (objTwo.current.uvi);
+          uvIndexEl.textContent = "UV Index: " + currentUVI;
+          // currentUVI.style.border = "green" NEED TO MAKE CURRENTUVI LOOK LIKE A BTN//
       })
 
       let currentDate = moment.unix(data.dt).format("MMMM Do, YYYY");
       let weatherIcon = data.weather[0].icon;
       let weatherImg = document.getElementById("weather-icon");
-
       weatherImg.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherIcon + ".png");
       weatherImg.style.width = "70px";
       weatherImg.style.height = "70px";
@@ -41,9 +44,11 @@ function getForecast(event) {
       currentTempEl.textContent = "Temp: " + data.main.temp + " °F";
       windSpeedEl.textContent = "Wind: " + data.wind.speed + " Mph";
       humidityEl.textContent = "Humidity: " + data.main.humidity + "%";
-    });
+      
+    })
 }
 searchBtn.addEventListener("click", getForecast);
-
+//NEED TO PULL IN 5DAY FORECAST & REMOVE THE HTML TEXT
+//NEED TO SET LOCAL STORAGE AND GET THE CITY NAMES TO SAVE UNDER THE SEARCH BAR
 
 

6173d312c9a0c0cd0b177d461fe7f65969e2e182
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-05-27 12:31:44 -0600

    appended 5day in js

--- scripts/script.js
+++ scripts/script.js
@@ -6,8 +6,48 @@ const currentTempEl = document.getElementById("current-temp");
 const windSpeedEl = document.getElementById("wind-mph");
 const humidityEl = document.getElementById("humid");
 const uvIndexEl = document.getElementById("uv-number");
-const fiveDayEl = document.getElementById("day-box");
+const fiveDayEl = document.getElementById("fiveday-forecast");
 
+function fiveDayForecast(lat, lon) {
+  let currentUVI 
+      var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${APIKey}&units=imperial&cnt=5`;
+      fetch(requestUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          let objTwo = data
+          currentUVI = (objTwo.current.uvi);
+          uvIndexEl.textContent = "UV Index: " + currentUVI;
+          // currentUVI.style.border = "green" NEED TO MAKE CURRENTUVI LOOK LIKE A BTN//
+          // console.log(data.daily[0])
+          // fiveDayTemp.textContent = "Temp: " + data.daily[0].temp.day;
+          // console.log(fiveDayTemp)
+          for (let i = 1; i < 6; i++) {
+            var box = document.createElement("div")
+            box.id = "daybox"
+            var date = document.createElement("h3")
+            date.textContent = moment.unix(data.daily[i].dt).format("MMMM Do, YYYY");
+            box.appendChild(date)
+            var icon = document.createElement("img")
+            icon.src = "https://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png"
+            box.appendChild(icon)
+            var temp = document.createElement("p")
+            temp.textContent = "Temp: " + data.daily[i].temp.day
+            box.appendChild(temp)
+            var wind = document.createElement("p")
+            wind.textContent = "Wind: " + data.daily[i].wind_speed
+            box.appendChild(wind)
+            var humid = document.createElement("p")
+            humid.textContent = "Humidity: " + data.daily[i].humidity
+            box.appendChild(humid)
+            fiveDayEl.appendChild(box)
+            console.log(box);
+            
+          }
+      })
+}
 
 function getForecast(event) {
   event.preventDefault();
@@ -19,19 +59,6 @@ function getForecast(event) {
     })
     .then(function (data) {
       console.log(data);
-      let currentUVI 
-      var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude={part}&appid=${APIKey}&units=imperial&cnt=5`;
-      fetch(requestUrl)
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (data) {
-          console.log(data);
-          let objTwo = data
-          currentUVI = (objTwo.current.uvi);
-          uvIndexEl.textContent = "UV Index: " + currentUVI;
-          // currentUVI.style.border = "green" NEED TO MAKE CURRENTUVI LOOK LIKE A BTN//
-      })
 
       let currentDate = moment.unix(data.dt).format("MMMM Do, YYYY");
       let weatherIcon = data.weather[0].icon;
@@ -44,7 +71,7 @@ function getForecast(event) {
       currentTempEl.textContent = "Temp: " + data.main.temp + " °F";
       windSpeedEl.textContent = "Wind: " + data.wind.speed + " Mph";
       humidityEl.textContent = "Humidity: " + data.main.humidity + "%";
-      
+      fiveDayForecast(data.coord.lat, data.coord.lon)
     })
 }
 searchBtn.addEventListener("click", getForecast);

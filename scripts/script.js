c9708ed3e0ecfe331ec56ec5e18a97973e2a5c97
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-05-27 18:09:24 -0600

    added if else statement for UVI

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@ const cityDateIconEl = document.getElementById("city-date-icon");
 const currentTempEl = document.getElementById("current-temp");
 const windSpeedEl = document.getElementById("wind-mph");
 const humidityEl = document.getElementById("humid");
-const uvIndexEl = document.getElementById("uv-number");
+// const uvIndexEl = document.getElementById("uv-number");
 const fiveDayEl = document.getElementById("fiveday-forecast");
 const fiveDayTitleEl = document.getElementById("fiveday-title");
 const citySearchList = JSON.parse(localStorage.getItem("cityData")) || [];
@@ -13,20 +13,18 @@ const citySearchList = JSON.parse(localStorage.getItem("cityData")) || [];
 function renderCitySearch() {
   for (let i = 0; i < citySearchList.length; i++) {
     let citiesSearched = citySearchList[i];
-    
     let pastSearchEl = document.getElementById("past-searched-items");
     let savedCityBtn = document.createElement("button");
+    //adding elements under the search bar of the users recently searched cities
     savedCityBtn.id = "saved-city-btn"
     savedCityBtn.textContent = citiesSearched;
     pastSearchEl.append(savedCityBtn);
     savedCityBtn.style.textTransform = "capitalize";
-
     savedCityBtn.setAttribute("value", citiesSearched);
-
+    //displays the cities searched after a browser refresh
     savedCityBtn.addEventListener("click", function () {
       getForecast(this.value);
     })
-
   }
 }
 
@@ -40,34 +38,34 @@ function getForecast(event) {
     citySearchList.push(city);
     localStorage.setItem("cityData", JSON.stringify(citySearchList));
   }
-
-    let queryURL ="https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
+    //declaring parameters and fetching data from API
+  let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
+  
   fetch(queryURL)
     .then(function (response) {
-      if (response.ok) {
+      if (!response.ok) {
+        alert("Error: " + response.statusText);
       }
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
 
       let currentDate = moment.unix(data.dt).format("MMMM Do, YYYY");
       let weatherIcon = data.weather[0].icon;
       let weatherImg = document.getElementById("weather-icon");
+      //pulling data values for current forecast into elements
       weatherImg.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherIcon + ".png");
       weatherImg.style.width = "70px";
       weatherImg.style.height = "70px";
-
       cityDateIconEl.textContent = data.name + ":"  + " " + currentDate;
       currentTempEl.textContent = "Temp: " + data.main.temp + "°F";
       windSpeedEl.textContent = "Wind: " + data.wind.speed + " MPH";
       humidityEl.textContent = "Humidity: " + data.main.humidity + "%";
-      
+      //calling function that pulls 5-day forecast when the getForecast function is called
       fiveDayForecast(data.coord.lat, data.coord.lon)
-
+      //clears the div elements when user searches a new city without refreshing the browser
       fiveDayEl.innerHTML = "";
       fiveDayTitleEl.innerHTML = "";
-
     })
 }
 
@@ -78,41 +76,54 @@ function fiveDayForecast(lat, lon) {
           return response.json();
         })
         .then(function (data) {
-          console.log(data);
-          let currentUVI = (data.current.uvi);
+          //pulling UVI data and adding it to current forecast
+          let uvIndexEl = document.getElementById("uv-number");
+          let currentUVI = data.current.uvi;
           uvIndexEl.textContent = "UV Index: " + currentUVI;
 
-          var title = document.createElement("h2");
+          if (currentUVI < 3 || currentUVI === 0) {
+                    currentUVI.setAttribute = ("style", "backgroundColor: green")
+          } else if (currentUVI >= 3 && currentUVI < 6) {
+                    currentUVI.setAttribute = ("style", "backgroundColor: yellow")
+          } else {
+                  currentUVI.setAttribute = ("style", "backgroundcolor: red")
+          }
+          
+          //creating the 5-day forecast title and adding it to parent div
+          let title = document.createElement("h2");
           title.textContent = "5-Day Forecast: ";
           fiveDayTitleEl.appendChild(title);
-          // currentUVI.style.border = "green" NEED TO MAKE CURRENTUVI LOOK LIKE A BTN//
+
+          //for loop that will run 5x to display the 5-day forecast
           for (let i = 1; i < 6; i++) {
             var boxFiveDay = document.createElement("div");
-            boxFiveDay.id = "daybox";
             var date = document.createElement("h3");
-            date.textContent = moment.unix(data.daily[i].dt).format("ddd. MMMM D");
-            boxFiveDay.appendChild(date);
-            
             var icon = document.createElement("img");
-            icon.id = "five-day-img";
-            icon.src = "https://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png";
-            boxFiveDay.appendChild(icon);
-
             var temp = document.createElement("p")
-            temp.textContent = "Temp: " + data.daily[i].temp.day + "°F";
-            boxFiveDay.appendChild(temp)
             var wind = document.createElement("p")
-            wind.textContent = "Wind: " + data.daily[i].wind_speed + " MPH";
-            boxFiveDay.appendChild(wind)
             var humid = document.createElement("p")
+            boxFiveDay.id = "daybox";
+            icon.id = "five-day-img";
+            //adding data values and text to the elements
+            date.textContent = moment.unix(data.daily[i].dt).format("ddd. MMMM D");
+            icon.src = "https://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png";
+            temp.textContent = "Temp: " + data.daily[i].temp.day + "°F";
+            wind.textContent = "Wind: " + data.daily[i].wind_speed + " MPH";
             humid.textContent = "Humidity: " + data.daily[i].humidity + "%";
-            boxFiveDay.appendChild(humid)
-            fiveDayEl.appendChild(boxFiveDay)
+            //adding these elements to the parent elements
+            boxFiveDay.appendChild(date);
+            boxFiveDay.appendChild(icon);
+            boxFiveDay.appendChild(temp);
+            boxFiveDay.appendChild(wind);
+            boxFiveDay.appendChild(humid);
+            fiveDayEl.appendChild(boxFiveDay);
           }
       })
 }
+//This function will display a list of the cities that the user has previous searched.
 renderCitySearch();
 
+//when the user clicks "search"--> everything above displays within the browser.
 searchBtn.addEventListener("click", getForecast);
 
 
